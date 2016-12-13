19604f64407c2aa7cef77846b9f1c14387032883
rodgerwatercompany1 <rebornyschen@gmail.com>
2016-12-13 17:32:19 +0800

    CreateProject.

---
+++ unpackPicture/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace unpackPicture
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Load";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 54);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Output";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(189, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "A simple tool to unpack tpsheet picture.";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 100);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "外公";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "tpsheet",
+            "font"});
+            this.comboBox1.Location = new System.Drawing.Point(110, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 20);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.Text = "font";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(336, 136);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ unpackPicture/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml;
+
+
+namespace unpackPicture
+{
+    public struct TileObject
+    {
+        public string FileName;
+        public Rectangle rect;    
+        public TileObject(string _filename,Rectangle _rect)
+        {
+            FileName = _filename;
+            rect = _rect;
+        }
+    }
+
+    public partial class Form1 : Form
+    {
+        Bitmap myBitmap = null;
+        List<TileObject> TileObjects;
+        string SavePath;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedItem.ToString() == "tpsheet")
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "BMP File| *.png";
+                openFileDialog1.Title = "取得影像檔";
+                openFileDialog1.FilterIndex = 3;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    myBitmap = new Bitmap(openFileDialog1.FileName);
+                }
+
+                string name = Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
+                string file = Path.GetDirectoryName(openFileDialog1.FileName);
+                string path = "";
+
+                SavePath = file + "\\unpack" + name + "\\";
+                path = file + "\\" + name + ".tpsheet";
+
+
+
+                TileObjects = new List<TileObject>();
+                string[] lines = System.IO.File.ReadAllLines(path);
+
+                Point pSize = new Point(myBitmap.Width, myBitmap.Height);
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string[] temp = lines[i].Split(';');
+                    if (temp.Length == 7)
+                    {
+                        int w = int.Parse(temp[3]);
+                        int h = int.Parse(temp[4]);
+                        int x = int.Parse(temp[1]);
+                        int y = pSize.Y - int.Parse(temp[2]) - h;
+
+                        TileObject tileObj = new TileObject(_filename: temp[0], _rect: new Rectangle(x, y, w, h));
+                        TileObjects.Add(tileObj);
+                    }
+                }
+            }
+            else if(comboBox1.SelectedItem.ToString() == "font")
+            {
+                OpenFileDialog openFileDialog1 = new OpenFileDialog();
+                openFileDialog1.InitialDirectory = "c:\\";
+                openFileDialog1.Filter = "BMP File| *.png";
+                openFileDialog1.Title = "取得影像檔";
+                openFileDialog1.FilterIndex = 3;
+                if (openFileDialog1.ShowDialog() == DialogResult.OK)
+                {
+                    myBitmap = new Bitmap(openFileDialog1.FileName);
+                }
+
+                string name = Path.GetFileNameWithoutExtension(openFileDialog1.FileName);
+                string file = Path.GetDirectoryName(openFileDialog1.FileName);
+                string path = "";
+
+                SavePath = file + "\\unpack" + name + "\\";
+                path = file + "\\" + name + ".fnt";
+                XmlDocument doc = new XmlDocument();
+                doc.Load(path);
+
+                XmlNode main = doc.SelectSingleNode("font/chars");//選擇節點
+                if (main == null)
+                    return;                
+
+                XmlElement element = (XmlElement)main;
+
+                TileObjects = new List<TileObject>();
+                for (int i = 0; i < element.ChildNodes.Count; i++)
+                {
+                    int x = Convert.ToInt32(element.ChildNodes[i].Attributes["x"].Value);
+                    int y = Convert.ToInt32(element.ChildNodes[i].Attributes["y"].Value);
+                    int width = Convert.ToInt32(element.ChildNodes[i].Attributes["width"].Value);
+                    int height = Convert.ToInt32(element.ChildNodes[i].Attributes["height"].Value);                    
+
+                    TileObject tileObj = new TileObject(_filename: i.ToString(), _rect: new Rectangle(x, y, width, height));
+                    TileObjects.Add(tileObj);
+                }
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (myBitmap != null)
+                {
+
+                    if (!System.IO.Directory.Exists(SavePath))
+                    {
+                        System.IO.Directory.CreateDirectory(SavePath);
+                        for (int i = 0; i < TileObjects.Count; i++)
+                        {
+                            Bitmap imageOutput = myBitmap.Clone(TileObjects[i].rect,
+                                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                            imageOutput.Save(SavePath + TileObjects[i].FileName + ".png", System.Drawing.Imaging.ImageFormat.Png);
+                            imageOutput.Dispose();
+                        }
+                        MessageBox.Show("unpack Complete !");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Error : Have unpacked !");
+
+                    }
+
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+       
+    }
+}
